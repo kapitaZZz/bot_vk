f0bb04d308078350e6ed01281a906edba86c4cd7
kapitaZZz <70171839+kapitaZZz@users.noreply.github.com>
2021-01-23 01:45:01 +0300

    Update config.py
    change config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # token file
 
-TOKEN = '88d2ffdeff877eaa46bac3afc7a73b734a15b04c518efdb0878d26f0df75de4b41c709aa0d8d0b585e7a3'
-group_id = '201744395'
+TOKEN = 'type here your group token'
+group_id = 'type here your group id'
